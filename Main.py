4aec3ae99da84aa8661e2f60193fad17d9268653
202317b3048-jpg <202317b3048@wilp.bits-pilani.ac.in>
2026-04-17 18:39:05 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,43 +1,23 @@
 import streamlit as st
-
 import pandas as pd
-
 st.title("Real Estate Market Analyzer Vijayawada & Madural")
-
 Sidebar selection
-
 cities ["Vijayawada", "Madural"]
-
 selected city st.sidebar.selectbox("select a city:", cities)
-
 Load dataset based on selection
-
 def load data(city):
-
 if city "Vijayawada":
-
 return pd.read_csv("Vijayawada.csv")
-
 #exact filename
-
 elser
-
 return pd.read_csv("Madurai.csv")
-
 exact filename
-
 df load data(selected_city)
-
 show dataset
-
 st.subheader("Dataset for (selected_city)")
-
 st.dataframe(df)
-
 Summary statistics
-
 st.subheader("Summary Statistics")
-
-25 st.write(df.describe())
+st.write(df.describe())
  
  
