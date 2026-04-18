8df0313a493c2f069065a4bbf6f4d47a91a61f7b
202317b3048-jpg <202317b3048@wilp.bits-pilani.ac.in>
2026-04-18 19:43:49 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,23 +1,24 @@
 import streamlit as st
 import pandas as pd
-st.title("Real Estate Market Analyzer Vijayawada & Madural")
-Sidebar selection
-cities ["Vijayawada", "Madural"]
-selected city st.sidebar.selectbox("select a city:", cities)
-Load dataset based on selection
-def load data(city):
-if city "Vijayawada":
-return pd.read_csv("Vijayawada.csv")
-#exact filename
-elser
-return pd.read_csv("Madurai.csv")
-exact filename
-df load data(selected_city)
-show dataset
-st.subheader("Dataset for (selected_city)")
+
+st.title("Real Estate Market Analyzer Vijayawada & Madurai")
+
+#Sidebar selection
+cities = ["Vijayawada", "Madurai"]
+selected_city = st.sidebar.selectbox("Select a city:", cities)
+
+#Load dataset based on selection
+def load_data(city):
+    if city == "Vijayawada":
+ return pd.read_csv("Vijayawada.csv") #exact filename
+    else:
+       return pd.read_csv("Madurai.csv") #exact filename
+ df =  load_data(selected_city)
+ #Show dataset
+st.subheader(f"Dataset for (selected_city)")
 st.dataframe(df)
-Summary statistics
-st.subheader("Summary Statistics")
+
+#Summary statistics
+st.subheader("Summary Statiics")
 st.write(df.describe())
- 
  
